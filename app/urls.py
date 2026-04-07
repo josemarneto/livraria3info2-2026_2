6c82b8b864f06fb203f402275514672e9b4f72c8
josemarneto <jc.mpento@gmail.com>
2026-04-07 14:37:57 -0300

    feat: add uploader app for handling document and image uploads
    - Created uploader app with models for Document and Image.
    - Implemented serializers for document and image uploads with validation.
    - Added viewsets for handling upload requests.
    - Registered models in Django admin.
    - Updated pyproject.toml for post-migrate command to include uploader.
    - Added helper functions for file type detection.
    - Created initial migration for Document and Image models.

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -20,6 +22,7 @@ from core.views import (
     UserRegistrationView,
     UserViewSet,
 )
+from uploader.router import router as uploader_router
 
 router = DefaultRouter()
 
@@ -31,6 +34,7 @@ router.register(r'livro', LivroViewSet, basename='livro')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/media/', include(uploader_router.urls)),
     # OpenAPI 3
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path(
@@ -52,3 +56,5 @@ urlpatterns = [
     # API
     path('api/', include(router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
